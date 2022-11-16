9e6a555848538bb5fdd06533f4e059fa94effd2b
André Mendes <develmendes@gmail.com>
2022-11-16 20:07:25 -0300

    script hamburguer active

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
-// Todo
+// Js Todo
+
 // Select hamburguer
+const hamburguer = document.querySelector('.hamburguer');
 // Select nav-menu
+const navMenu = document.querySelector('.nav-menu');
 // Add event listener
+hamburguer.addEventListener('click', mobileMenu);
 // Navmenu function
 // Toggle active
+function mobileMenu() {
+  hamburguer.classList.toggle('active');
+  navMenu.classList.toggle('active');
+}
